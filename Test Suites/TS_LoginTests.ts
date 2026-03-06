1da1a343c57c99f972d1f01c604c01c0122b4637
quanghunh <nhatquangh72@gmail.com>
2026-03-06 09:12:58 +0700

    a

---
+++ Test Suites/TS_LoginTests.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_LoginTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ef04f05c-9b34-4905-b6bb-f0e26b28eb6c</testSuiteGuid>
+</TestSuiteEntity>
